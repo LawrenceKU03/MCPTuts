434961c9060848174dd0cc013ab9e2c56a170d25
Lawrence <lawuche29@gmail.com>
2026-07-18 00:12:05 +0100

    chore:fastmcp is awesome

---
+++ server.py
@@ -0,0 +1,13 @@
+
+from fastmcp import FastMCP 
+
+mcp=FastMCP(name="calculator")
+
+@mcp.tool(name="multiply",description="a function that takes two numbers and multiply them a:float b:float and returns the product of a * b which is of type float",tags=["maths","arithmetics"])
+def multiply(a:float,b:float)-> float:
+    return  a*b
+
+
+
+if __name__ ==" __main__":
+    mcp.run()
